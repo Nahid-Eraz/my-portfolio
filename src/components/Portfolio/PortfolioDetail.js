90663fa9b3ba79cd8681ea7d75254a4f7e671c89
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-02 20:35:41 +0600

    portfolio hover

--- src/components/Portfolio/PortfolioDetail.js
+++ src/components/Portfolio/PortfolioDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PortfolioDetail.css'
 import { Button, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faLink} from '@fortawesome/free-solid-svg-icons'
@@ -7,7 +8,7 @@ const PortfolioDetail = (props) => {
     const { name, img, tech, live } = props.portfolio;
     return (
         <div className='col-md-4 text-center mt-3 p-4 d-flex align-items-center justify-content-around shadow-lg p-3 mb-5 bg-body rounded'>
-            <Card style={{ width: '15rem' }}>
+            <Card className='card-style'>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
