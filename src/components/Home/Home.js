bf9a0521d5ceb0dfb4dd88f90a95531f9fafe3e4
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-02 15:45:44 +0600

    home finalizing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,9 +10,22 @@ const Home = () => {
                     <img className='img-fluid' src={myImg} alt="" />
                 </div>
                 <div className="col-md-4">
-                    <h1 style={{color: 'white'}} className="heading-text">- I'm Nahid Eraz</h1>
-                    <h1 style={{color: 'white'}}>Front-End Web Developer</h1>
-                    <p style={{color: 'lightGrey'}}> Focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                    <h1 style={{ color: 'white' }} className="heading-text">- I'm Nahid Eraz</h1>
+                    <h1 style={{ color: 'white' }}>Front-End Web Developer</h1>
+                    <p style={{ color: 'lightGrey' }}> Focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+
+                    <div className='d-flex'>
+                    <div class="buttons">
+                        <div class="container">
+                            <a href="#" class="btn effect01" target="_blank"><span>Download Resume</span></a>
+                        </div>
+                    </div>
+                    <div class="buttons">
+                        <div class="container">
+                            <a href="/about" class="btn effect01" target="_blank"><span>More About Me</span></a>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
