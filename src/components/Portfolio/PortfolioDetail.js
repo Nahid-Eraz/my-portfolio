554ce2545380a3cdfc4ca97d2371218112b08bc1
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-02 20:30:58 +0600

    portfolio done

--- src/components/Portfolio/PortfolioDetail.js
+++ src/components/Portfolio/PortfolioDetail.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faLink} from '@fortawesome/free-solid-svg-icons'
 
 const PortfolioDetail = (props) => {
-    const {name, img, tech, live} = props.portfolio
+    const { name, img, tech, live } = props.portfolio;
     return (
-        <div>
-            
+        <div className='col-md-4 text-center mt-3 p-4 d-flex align-items-center justify-content-around shadow-lg p-3 mb-5 bg-body rounded'>
+            <Card style={{ width: '15rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>{tech}</Card.Text>
+                    <div class="buttons">
+                        <div class="container">
+                            <a href={live} class="btn effect01"><span>Live Link <FontAwesomeIcon icon={faLink} /></span></a>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
