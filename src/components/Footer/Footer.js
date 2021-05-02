bee6c64f2c973e813e8e2b60a6ab63a15b7d55ea
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-02 23:21:29 +0600

    check again

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,11 +10,11 @@ const Footer = () => {
                 <p>Follow Me</p>
                 <div className="row">
                     <ul className="media list-inline">
-                        <li className="list-inline-item"><a href="https://www.facebook.com/erazpro/"><FontAwesomeIcon className="icon" icon={faFacebookF} /></a></li>
-                        <li className="list-inline-item"><a href="https://twitter.com/eraz_md"><FontAwesomeIcon className="icon" icon={faTwitter} /></a></li>
-                        <li className="list-inline-item"><a href="https://github.com/Nahid-Eraz"><FontAwesomeIcon className="icon" icon={faGithub} /></a></li>
-                        <li className="list-inline-item"><a href="https://www.linkedin.com/in/nahid-eraz-55a3811b5/"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a></li>
-                        <li className="list-inline-item"><a href="https://medium.com/@nahidulislameraz"><FontAwesomeIcon className="icon" icon={faMedium} /></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/erazpro/"><FontAwesomeIcon className="icon" icon={faFacebookF} /></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/eraz_md"><FontAwesomeIcon className="icon" icon={faTwitter} /></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noopener noreferrer" href="https://github.com/Nahid-Eraz"><FontAwesomeIcon className="icon" icon={faGithub} /></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nahid-eraz-55a3811b5/"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@nahidulislameraz"><FontAwesomeIcon className="icon" icon={faMedium} /></a></li>
                     </ul>
                 </div>
                 <div className="text-center">
