ab5a2b928425ccd1b347f018d369eb4b4f662299
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-05-03 20:55:57 +0600

    little update in contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
             <div className="m-3">
                 <h2 style={{ color: 'white' }} className="text-center">GET IN TOUCH</h2>
                 <div className="row d-flex justify-content-center mt-5">
-                    <div className='col-md-3 me-5'>
+                    <div className='col-md-3 me-5 p-3'>
                         <h2>Wanna Work Together</h2>
                         <h6 className="mt-4">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</h6>
                         <div className='container d-flex justify-content-evenly'>
